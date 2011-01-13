3399d22a2cd0e18a53bb470902f1a317d9104e8f
Larry Garfield <garfield@palantir.net>
2011-01-13 16:19:45 -0600

    Verify that we can properly fork to child processes to handle queue processing.  This code will probably all get thrown out, but it's a proof of concept of the general idea.  Nifty.

--- test-pnctl.php
+++ test-pnctl.php
@@ -1,28 +1,81 @@
 <?php
 
-// Load config file.
-$config = array(
-  'first' => 'Hello',
-  'second' => 'World',
-);
+foreach (range(0, 10) as $i) {
+  $messages[] = "Hello World #" . $i;
+}
 
-$num_children = 1;
+$config['messages'] = $messages;
+$config['num_children'] = 2;
 
-for ($i = 0; $i < $num_children; ++$i) {
+require_once('pheanstalk/pheanstalk_init.php');
+
+for ($i = 0; $i < $config['num_children']; ++$i) {
   $pid = pcntl_fork();
   if ($pid == -1) {
     die('could not fork');
   } else if ($pid) {
     // we are the parent
     //pcntl_wait($status); //Protect against Zombie children
+
   } else {
     // we are the child
-    print "We are in the child." . PHP_EOL;
-    sleep(1);
-    print "Exiting child." . PHP_EOL;
+    print "Creating child #{$i}" . PHP_EOL;
+    child_process($config, $i);
     exit();
   }
 }
 
-
+parent_process($config);
 print "Parent ending..." . PHP_EOL;
+
+
+function parent_process($config) {
+
+  $pheanstalk = new Pheanstalk('beanstalk.palantir.net');
+
+  $pheanstalk->useTube('messages');
+
+  // Mostly just for debugging.
+  print_r($pheanstalk->statsTube('messages'));
+
+  foreach ($config['messages'] as $message) {
+    $pheanstalk->put($message, 10);
+    // Slow down the queuing process so that we can test the system better.
+    // Obviously the real one won't have this.
+    sleep(1);
+  }
+
+  for ($i = 0; $i < $config['num_children']; ++$i) {
+    $pheanstalk->put('KILL', 20);
+  }
+
+}
+
+function child_process($config, $id) {
+  $pheanstalk = new Pheanstalk('beanstalk.palantir.net');
+
+  while (true) {
+    // This will block until there is a job to process.
+    // If we wanted to process jobs continually, we would put this in a while loop.
+    $job = $pheanstalk
+      ->watch('messages')
+      ->ignore('default')
+      ->reserve();
+
+    $data = $job->getData();
+
+    if ($data == 'KILL') {
+      echo "KILL: Child {$id} is about to commit sepuku." . PHP_EOL;
+      $pheanstalk->delete($job);
+      break;
+    }
+    else {
+      echo "Child {$id} says: {$data}" . PHP_EOL;
+      $pheanstalk->delete($job);
+    }
+  }
+
+
+  //print "We are in the child." . PHP_EOL;
+  //print "Child: {$config['first']} {$config['second']}." . PHP_EOL;
+}
