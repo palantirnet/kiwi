ea947cae8f2c3dbc856f9fef52cf71af20c41b9f
Larry Garfield <garfield@palantir.net>
2011-02-16 11:27:08 -0600

    refs #1324 Add utility library for random stuff.  Hopefully this won't get too big.

--- kiwi.php
+++ kiwi.php
@@ -19,6 +19,13 @@ date_default_timezone_set('America/Chicago');
 IMuTrace::setFile('trace.txt');
 IMuTrace::setLevel(1);
 
+// Include teh Kiwi libraries.
+require_once('lib/util.inc');
+require_once('lib/KiwiConfiguration.inc');
+require_once('lib/KiwiQueryGenerator.inc');
+require_once('lib/KiwiWorker.inc');
+
+
 function main() {
   $config = new KiwiConfiguration('');
 
@@ -28,92 +35,4 @@ function main() {
 
 }
 
-
-/**
- * Configuration object for a Kiwi execution run.
- */
-class KiwiConfiguration {
-
-  /**
-   * Constructor.
-   *
-   * @param string $file
-   *   The name of the file with the configuration to load.
-   */
-  function __construct($file = '') {
-
-  }
-
-}
-
-
-class KiwiQueryGenerator {
-
-  /**
-   * The configuration object for this generator.
-   *
-   * @var KiwiConfiguration
-   */
-  protected $config;
-
-  public function __construct(KiwiConfiguration $config) {
-    $this->config = $config;
-  }
-
-  public function run() {
-    // Connect to Emu server.
-
-    // Run initial query as defined by the config object.
-    // This may require a query builder.
-
-    // Return the module ID
-  }
-
-}
-
-class KiwiWorker {
-
-  /**
-   * The configuration object for this generator.
-   *
-   * @var KiwiConfiguration
-   */
-  protected $config;
-
-  /**
-   * The module ID we should load.
-   *
-   * This is a unique identifier for the result set object in Emu.
-   *
-   * @var string
-   */
-  protected $moduleId = '';
-
-  public function __construct(KiwiConfiguration $config, $module_id) {
-    $this->config = $config;
-    $this->moduleId = $module_id;
-
-    // Create $this->module object.
-
-    // Create $this->solrConnection object.
-  }
-
-  public function run() {
-    // Create $solr_document.
-
-    // Create field list for all possible fields to fetch. This includes from
-    // the base table and 1:1 mappings (Append).
-
-    // Extract those fields.
-
-    // Add those fields to $solr_document.
-
-    // Process table fields and add those to $solr_document.
-
-    // Process Merge tables.
-
-    // Process Collapse tables.
-
-  }
-
-}
+main();
