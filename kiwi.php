1b0ad22677898c57937123a7af04edf958a07c7d
Larry Garfield <garfield@palantir.net>
2011-02-16 16:39:09 -0600

    refs #1384 Never mind, we'll use the PEAR version of QueryPath.

--- kiwi.php
+++ kiwi.php
@@ -1,35 +1,42 @@
 #!/usr/bin/php
 <?php
 
-/* First we'll import the base API library
-*/
-require_once 'emu/imu.php';
-/* Next we'll import the module library, as that will
-** provide useful tools for querying and returning
-** results
-*/
-require_once IMu::$lib . '/module.php';
-require_once IMu::$lib . '/modules.php';
-/* We don't really need these, but they can be useful
-*/
-require_once IMu::$lib . '/exception.php';
-require_once IMu::$lib . '/trace.php';
 
-date_default_timezone_set('America/Chicago');
 
-IMuTrace::setFile('trace.txt');
-IMuTrace::setLevel(1);
 
-// Include the Kiwi libraries.
-require_once('lib/util.inc');
-require_once('lib/KiwiConfiguration.inc');
-require_once('lib/KiwiQueryGenerator.inc');
-require_once('lib/KiwiWorker.inc');
-require_once('lib/KiwiImuSession.inc');
 
+function includeEmu() {
+  require_once 'emu/imu.php';
+  require_once IMu::$lib . '/module.php';
+  require_once IMu::$lib . '/modules.php';
+  require_once IMu::$lib . '/exception.php';
+  require_once IMu::$lib . '/trace.php';
+}
+
+function includeKiwi() {
+  // Include the Kiwi libraries.
+  require_once('lib/util.inc');
+  require_once('lib/KiwiConfiguration.inc');
+  require_once('lib/KiwiQueryGenerator.inc');
+  require_once('lib/KiwiWorker.inc');
+  require_once('lib/KiwiImuSession.inc');
+}
+
+function includeQueryPath() {
+
+}
 
 function main() {
-  $config = new KiwiConfiguration('');
+  includeEmu();
+  includeKiwi();
+  includeQueryPath();
+
+  IMuTrace::setFile('trace.txt');
+  IMuTrace::setLevel(1);
+
+  date_default_timezone_set('America/Chicago');
+
+  $config = new KiwiConfiguration('config.xml');
 
   $generator = new KiwiQueryGenerator($config);
 
