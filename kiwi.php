2fbe056d02c07bc027e32c36b291721fe552711c
Larry Garfield <garfield@palantir.net>
2011-02-22 18:42:26 -0600

    Cleanup the structure of the main routine a bit by breaking it into separate utility functions.

--- kiwi.php
+++ kiwi.php
@@ -31,6 +31,11 @@ function includeSolr() {
   require_once('SolrPhpClient/Apache/Solr/Response.php');
 }
 
+/**
+ * Main driver for the program.
+ *
+ * This is the main entry point for the whole script.
+ */
 function main() {
   includeEmu();
   includeKiwi();
@@ -44,18 +49,10 @@ function main() {
 
   $config = new KiwiConfiguration('config.xml');
 
-  $server_info = $config->getEmuInfo();
-  $session = new KiwiImuSession($config, $server_info['host'], $server_info['port']);
-
-  $generator = new KiwiQueryGenerator($config, $session);
-
-  $module_id = $generator->run();
+  // Build the main query on the Emu server.
+  $module_id = main_generator($config);
 
   debug($module_id, 'Module ID');
-
-  // Destroy the generator to get rid of all outstanding resource objects
-  // before we fork.
-  unset($generator);
 
   // Spawn off children to do work.
   for ($child_id = 1, $num_processors = $config->numChildProcesses(); $child_id <= $num_processors; ++$child_id) {
@@ -65,32 +62,47 @@ function main() {
     }
     else if ($pid) {
       // This is the parent.
+      pcntl_wait($status); //Protect against Zombie children.
+      main_cleanup($config);
     }
     else {
       // This is the child.
-      debug("Child {$child_id} running...");
-      $server_info = $config->getEmuInfo();
-      $session = new KiwiImuSession($config, $server_info['host'], $server_info['reconnect-port']);
-
-      $server_info = $config->getSolrInfo();
-      $solr = new Apache_Solr_Service($server_info['host'], $server_info['port'], $server_info['path']);
-
-      $processor = new KiwiQueryProcessor($child_id, $module_id, $config, $session, $solr);
-
-      //try {
-        $processor->run();
-      //}
-      //catch(Exception $e) {
-        //debug($e->getTrace());
-        //debug('Error message is: ' . $e->getMessage());
-        //debug('Error code is: ' . $e->getCode());
-      //}
+      main_processor($config, $child_id, $module_id);
+      // Kill the child process when done.
       exit(0);
     }
   }
 
-  // We are the parent.
-  pcntl_wait($status); //Protect against Zombie children
+  exit();
+}
+
+/**
+ * Generates a new query result on the Emu server and returns the module ID.
+ *
+ * @param KiwiConfiguration $config
+ *   The configuration object for this run.
+ * @return string
+ *   The Module ID of the result set object.
+ */
+function main_generator(KiwiConfiguration $config) {
+  $server_info = $config->getEmuInfo();
+  $session = new KiwiImuSession($config, $server_info['host'], $server_info['port']);
+
+  $generator = new KiwiQueryGenerator($config, $session);
+
+  return $generator->run();
+
+  // The generator object goes out of scope here, clearing all outstanding
+  // resources.
+}
+
+/**
+ * Runs the cleanup routines for after all child processes are done.
+ *
+ * @param KiwiConfiguration $config
+ *   The configuration object for this run.
+ */
+function main_cleanup(KiwiConfiguration $config) {
 
   // Close the result set object.
   // @todo I'm not sure if we need to, since the modules in the children may
@@ -106,8 +118,38 @@ function main() {
   $solr = new Apache_Solr_Service($server_info['host'], $server_info['port'], $server_info['path']);
   $solr->commit(TRUE, FALSE, FALSE);
 
+  //$solr->deleteByQuery('*:*');
 
-  exit();
+}
+
+/**
+ *
+ * @param KiwiConfiguration $config
+ *   The configuration object for this run.
+ * @param int $child_id
+ *   The ID of this processor. It is a simple integer, unique within this
+ *   script run only.
+ * @param string $module_id
+ *   The Module ID of the result set object on whic to work.
+ */
+function main_processor(KiwiConfiguration $config, $child_id, $module_id) {
+  debug("Child {$child_id} running...");
+  $server_info = $config->getEmuInfo();
+  $session = new KiwiImuSession($config, $server_info['host'], $server_info['reconnect-port']);
+
+  $server_info = $config->getSolrInfo();
+  $solr = new Apache_Solr_Service($server_info['host'], $server_info['port'], $server_info['path']);
+
+  $processor = new KiwiQueryProcessor($child_id, $module_id, $config, $session, $solr);
+
+  //try {
+    $processor->run();
+  //}
+  //catch(Exception $e) {
+    //debug($e->getTrace());
+    //debug('Error message is: ' . $e->getMessage());
+    //debug('Error code is: ' . $e->getCode());
+  //}
 }
 
 main();
