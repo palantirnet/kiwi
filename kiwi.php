5a638e2a3ba4de3794f11a67b815fb0ed6be9003
Larry Garfield <garfield@palantir.net>
2011-04-19 19:13:07 -0500

    refs #1388 Catch exceptions in the main routine.

--- kiwi.php
+++ kiwi.php
@@ -53,55 +53,63 @@ function main() {
 
   $input = new KiwiInput();
 
-  $config = new KiwiConfiguration($input);
+  try {
+    $config = new KiwiConfiguration($input);
 
-  $config_info = $config->getConfigInfo();
+    $config_info = $config->getConfigInfo();
 
-  KiwiOutput::get()->setThreshold($config_info['debug']);
+    KiwiOutput::get()->setThreshold($config_info['debug']);
 
-  // Build the main query on the Emu server.
-  $module_id = main_generator($config);
+    // Build the main query on the Emu server.
+    $module_id = main_generator($config);
 
-  KiwiOutput::debug($module_id, 'Module ID');
+    KiwiOutput::debug($module_id, 'Module ID');
 
-  // If specified, clear the existing Solr core before adding new content.
-  if ($config_info['full-rebuild']) {
-    $server_info = $config->getSolrInfo();
-    KiwiOutput::info("Purging old solr index...");
-    $solr = new Apache_Solr_Service($server_info['host'], $server_info['port'], $server_info['path']);
-    $solr->deleteByQuery('*:*');
-    unset($solr); // Close the connection.
-  }
-
-  $children = array();
-
-  // Spawn off children to do work.
-  for ($child_id = 1, $num_processors = $config->numChildProcesses(); $child_id <= $num_processors; ++$child_id) {
-    $pid = pcntl_fork();
-    if ($pid == -1) {
-      die('could not fork');
+    // If specified, clear the existing Solr core before adding new content.
+    if ($config_info['full-rebuild']) {
+      $server_info = $config->getSolrInfo();
+      KiwiOutput::info("Purging old solr index...");
+      $solr = new Apache_Solr_Service($server_info['host'], $server_info['port'], $server_info['path']);
+      $solr->deleteByQuery('*:*');
+      unset($solr); // Close the connection.
     }
-    else if ($pid) {
-      // This is the parent. Do nothing here but let the loop complete.
-      $children[] = $pid;
-    }
-    else {
-      // This is the child.
-      main_processor($config, $child_id, $module_id);
-      // Kill the child process when done.
-      exit(0);
-    }
-  }
 
-  // Wait for all of the spawned children to die.
-  // This may end up waiting for process 1 long after process 3 is done, but
-  // that's OK. It ensures that all processes are truly done before continuing,
-  // whatever order they finish in, which is what we want.
-  $status = 0;
-  foreach ($children as $pid) {
-   pcntl_waitpid($pid, $status);
+    $children = array();
+
+    // Spawn off children to do work.
+    for ($child_id = 1, $num_processors = $config->numChildProcesses(); $child_id <= $num_processors; ++$child_id) {
+      $pid = pcntl_fork();
+      if ($pid == -1) {
+        die('could not fork');
+      }
+      else if ($pid) {
+        // This is the parent. Do nothing here but let the loop complete.
+        $children[] = $pid;
+      }
+      else {
+        // This is the child.
+        main_processor($config, $child_id, $module_id);
+        // Kill the child process when done.
+        exit(0);
+      }
+    }
+
+    // Wait for all of the spawned children to die.
+    // This may end up waiting for process 1 long after process 3 is done, but
+    // that's OK. It ensures that all processes are truly done before continuing,
+    // whatever order they finish in, which is what we want.
+    $status = 0;
+    foreach ($children as $pid) {
+     pcntl_waitpid($pid, $status);
+    }
+    main_cleanup($config);
   }
-  main_cleanup($config);
+  catch (ConfigFileNotFoundException $e) {
+    KiwiOutput::get()->writeMessage($e->getMessage(), LOG_ERR);
+  }
+  catch (Exception $e) {
+    KiwiOutput::get()->writeMessage('Unknown error: ' . $e->getMessage(), LOG_ERR);
+  }
 
   exit();
 }
