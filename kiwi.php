0b421c3b7efb91792d46bcfdfb865574d5fb035e
Larry Garfield <garfield@palantir.net>
2011-06-14 18:27:39 -0500

    refs #1388 Add support for parsing a whole directory full of config files at once.

--- kiwi.php
+++ kiwi.php
@@ -5,5 +5,24 @@ require_once('bootstrap.php');
 
 kiwi_init();
 
-main();
+/**
+ * Initialization driver for the Kiwi script.
+ */
+function kiwi() {
+  $input = new KiwiInput();
+  $input->parse();
+
+  // Allow for parsing of an entire directory or just an individual file.
+  if ($directory = $input->getOption('directory')) {
+    foreach (glob($directory . '/*.xml') as $file) {
+      KiwiOutput::info("Parsing {$file} for processing.\n");
+      main($input, new KiwiConfiguration($input, $file));
+    }
+  }
+  else {
+    main($input);
+  }
+}
+
+kiwi();
 exit();
