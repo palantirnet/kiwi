9b2181eeaa69b02f4491f4eeb832fed3deb0d3b3
Larry Garfield <garfield@palantir.net>
2011-06-14 19:02:55 -0500

    refs #1388 Make a config file specification explicit for the command line.

--- kiwi.php
+++ kiwi.php
@@ -9,18 +9,28 @@ kiwi_init();
  * Initialization driver for the Kiwi script.
  */
 function kiwi() {
-  $input = new KiwiInput();
-  $input->parse();
+  try {
+    $input = new KiwiInput();
+    $input->parse();
 
-  // Allow for parsing of an entire directory or just an individual file.
-  if ($directory = $input->getOption('directory')) {
-    foreach (glob($directory . '/*.xml') as $file) {
-      KiwiOutput::info("Parsing {$file} for processing.\n");
-      main($input, new KiwiConfiguration($input, $file));
+    // Allow for parsing of an entire directory or just an individual file.
+    if ($directory = $input->getOption('directory')) {
+      foreach (glob($directory . '/*.xml') as $file) {
+        KiwiOutput::info("Parsing {$file} for processing.\n");
+        main($input, new KiwiConfiguration($input, $file));
+      }
+    }
+    else {
+      main($input);
     }
   }
-  else {
-    main($input);
+  catch (InvalidConfigOptionException $e) {
+    KiwiOutput::get()->writeMessage($e->getMessage(), LOG_ERR);
+    KiwiOutput::get()->writeMessage(PHP_EOL . PHP_EOL . $input->getInstructions());
+  }
+  catch (MissingConfigOptionException $e) {
+    KiwiOutput::get()->writeMessage($e->getMessage(), LOG_ERR);
+    KiwiOutput::get()->writeMessage(PHP_EOL . PHP_EOL . $input->getInstructions());
   }
 }
 
